874e9af1ef9df03f73d587a8662f2402813ab7f7
Brooklyn Kairupan <brokinignacio3@gmail.com>
2022-09-21 15:52:08 +0800

    add js

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const Header = document.querySelector('.header')
+const Button = document.querySelector(' .button')
+
+Header.style.backgroundColor ='#3252DF'
+
+Button.addEventListener('click', function(){
+    alert('Hello World');
+})
